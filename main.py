9561ffa267650f88ad9fe4d93e50089f1beae06d
x2-qp-cheese <pgw053024@naver.com>
2023-03-09 19:28:10 +0900

    for the assignment

---
+++ main.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target) :
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)) :
+            if nums[i]+nums[j] == target :
+                return [i,j]
+
+nums = list(map(int, input().split()))
+target = int(input())
+
+print(twoSum(nums, target))
